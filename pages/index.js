d9b32876a19ebd4387fd6e17ebbc7f4b99a51d31
22p21s0045 <monkeytitle@hotmail.com>
2022-04-30 23:05:22 +0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,16 @@ import styles from '../styles/Home.module.css';
 import liff from "@line/liff"
 import { Main } from 'next/dist/pages/_document';
 export default function Home() {
-  
+  async function main(){
+    liff.ready.then(()=>{
+      if(liff.getOS()=== "web"){
+        console.log("You are log in with pc")
+      }
+    })
+    await liff.init({liffId:"1657099369-78y1D9eA"})
+
+  }
+  main()
   return (
     <div >
       <h1> Hello index</h1>
